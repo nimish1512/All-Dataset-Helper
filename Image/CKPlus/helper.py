0b30058ef3ec0a672be4ffab79903d7d8e27cd37
Nimish <nimish.ronghe@flochat.in>
2018-04-07 14:17:27 +0530

    Added batch-support

--- Image/CKPlus/helper.py
+++ Image/CKPlus/helper.py
@@ -7,7 +7,10 @@ class CKLoader:
 		try:
 			target_class = os.listdir("sorted_set")
 			self.images = {}
-			self.classes = len(target_class)
+			self.encoded_classes = {}
+			for i in range(len(target_class)):
+				self.encoded_classes.update({target_class[i]:i})
+			#print(self.encoded_classes)
 			temp = list()
 			self.min_samples_count = 5
 			self.min_samples_class=None
@@ -25,15 +28,17 @@ class CKLoader:
 				self.images.update({cl:temp[0]})
 				del temp[:]
 			del flag
+			del temp
 		except Exception as e:
 			print(e)
 
-	def format_data(self,train_test_split=0.8, normalized_test_split=False, shuffle=False, channels=1, img_dims=(32,32)):
+	def format_data(self,train_test_split=0.8, normalized_test_split=False, shuffle=False, channels=1, img_dims=[32,32], preprocess=[], batch_size=128):
 		try:
 			train_images = list()
 			train_labels = list()
 			test_images = list()
-			test_labels = list()		
+			test_labels = list()
+			self.img_size= img_dims	
 			if normalized_test_split:
 				if int(self.min_samples_count*train_test_split)<=5:
 					raise Exception("Cannot split dataset. Test-data too small {0} test samples for class {1}".format(int(self.min_samples_count*train_test_split),self.min_samples_class))
@@ -46,53 +51,54 @@ class CKLoader:
 							train_count = len(v)-test_count
 							for i in range(train_count):
 								train_images.append(v[i])
-								train_labels.append(k)
+								train_labels.append(self.encoded_classes[k])
 							for i in range(len(v)-test_count,len(v)):
 								test_images.append(v[i])
-								test_labels.append(k)
+								test_labels.append(self.encoded_classes[k])
 			else:
 				for k,v in self.images.items():
 					train_count = int(len(v)*train_test_split)
 					for i in range(train_count):
 						train_images.append(v[i])
-						train_labels.append(k)
+						train_labels.append(self.encoded_classes[k])
 					for i in range(len(v)-train_count,len(v)):
 						test_images.append(v[i])
-						test_labels.append(k)
+						test_labels.append(self.encoded_classes[k])
+			train_images = tf.constant(train_images)
+			train_labels = tf.constant(train_labels)
+			train_labels = tf.cast(train_labels, tf.int32)
+			test_images = tf.constant(test_images)
+			test_labels = tf.constant(test_labels)
+			test_labels = tf.cast(test_labels, tf.int32)
 			training_data = tf.data.Dataset.from_tensor_slices((train_images,train_labels))
 			test_data = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
-			print(training_data.output_types)
-			training_data = training_data.map(load_images)
-			test_data = test_data.map(load_images)
-			iter1 = tf.data.Iterator.from_structure(training_data.output_types, training_data.output_shapes)
+			training_data = training_data.map(self.load_images)
+			test_data = test_data.map(self.load_images)
+			training_data = training_data.batch(batch_size)
+			iter1 = training_data.make_one_shot_iterator()
 			self.train_next_batch = iter1.get_next()
 		except Exception as e:
 			print(e)
 				
 
-def load_images(img_path, label):
-	one_hot = tf.one_hot(label, 8)
-	img = tf.read_file(img_path)
-	img = tf.image.decode_image(img, channels=1)
-	return (img,one_hot)
-
-
-
-x = CKLoader()
-print(x.min_samples_class, x.min_samples_count)
-x.format_data()
-
-'''
-images_list = []
-for i in range(6):
-    image = tf.read_file("{0}.jpg".format(i))
-    image_tensor = tf.image.decode_jpeg(image, channels=3)
-    image_tensor = tf.image.rgb_to_grayscale(image_tensor)
-    image_tensor = tf.image.resize_images(image_tensor, [28, 28])
-    image_tensor = tf.expand_dims(image_tensor, 0)
-    images_list.append(image_tensor)
+	def load_images(self, img_path, label):
+		one_hot = tf.one_hot(label, len(self.encoded_classes))
+		img = tf.read_file(img_path)
+		img = tf.image.decode_image(img, channels=1)
+		img.set_shape([None, None, None])
+		img = tf.image.resize_images(img, self.img_size)
+		return img,one_hot
 
 with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())	
-	batches = tf.train.batch(images_list, batch_size=3, enqueue_many=True, capacity=6)
-'''
+	x = CKLoader()
+	x.format_data(batch_size=128)
+	#sess.run(x.trainer_init)
+	i=1
+	while True:
+		try:
+			elem = sess.run(x.train_next_batch)
+			print(type(elem))
+			print("Batch {0} ------------------------------------------------------->".format(i))
+			i+=1
+		except:
+			break
